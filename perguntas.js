5caa6a0d30a8bc0b5a6459f15ce531c26ae83d29
Evaristo041 <gustavo.evaristo.santos@escola.pr.gov.br>
2025-06-11 19:58:29 +0000

    Finalizando com adição de java e algumas pequenas alterações no css e no html

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Muay Thai',
+    'Onde surgiu o Muay Thai?',
+    'O Muay Thai surgiu na Tailândia, há vários séculos, como forma de defesa pessoal e combate em guerras.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'Qual é o apelido tradicional do Muay Thai?',
+    'Ele é conhecido como a “Arte das Oito Armas”, por usar punhos, cotovelos, joelhos e canelas.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'O Muay Thai é considerado um esporte olímpico?',
+    'Ainda não é esporte olímpico oficial, mas está ganhando reconhecimento internacional e pode entrar nos próximos anos.'
+)
+
+criaCartao(
+    'Muay Thai',
+    ' Qual é a saudação tradicional feita antes das lutas de Muay Thai?',
+    'É o "Wai Kru Ram Muay", uma dança ritual em homenagem aos mestres e à tradição.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'Qual é a principal diferença entre o Muay Thai e outras artes marciais como o boxe?',
+    'O Muay Thai permite o uso de cotovelos, joelhos, clinch e chutes — muito mais completo que o boxe, que usa apenas os punhos.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'Quais são os equipamentos básicos para treinar Muay Thai com segurança?',
+    'Luvas, caneleiras, protetor bucal, bandagens, shorts apropriados e protetor genital.'
+)
+
+criaCartao(
+    'Muay Thai',
+    ' É verdade que lutadores tailandeses treinam descalços?',
+    'Sim, o treinamento é tradicionalmente feito descalço para fortalecer os pés e manter o contato com o solo.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'Muay Thai ajuda apenas na luta ou também na saúde?',
+    'Ajuda muito na saúde! Melhora o condicionamento físico, emagrece, reduz o estresse e aumenta a disciplina.'
+)
+
+criaCartao(
+    'Muay Thai',
+    'O Muay Thai é indicado apenas para homens?',
+    'Não! O Muay Thai é para todos — mulheres treinam, competem e se destacam cada vez mais nesse esporte.'
+)
